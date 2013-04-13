2100b0856c4b486eb781f5796aab2d241266ef91
Kacey of Minetest <ericbooth68@gmail.com>
2013-04-13 09:30:34 -0600

    register more things
    coming along quickly

--- init.lua
+++ init.lua
@@ -2,7 +2,28 @@ minetest.register_node("xytest:stone_with_ore", {
 	description = "Xychorium Ore",
 	tiles = {"default_stone.png^xytest_ore.png"},
 	is_ground_content = true,
-	groups = {cracky=3, not_in_creative_inventory=1},
+	groups = {cracky=3},
 	drop = 'xytest:xychorium',
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "xytest:stone_with_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 8*8*8,
+	clust_num_ores = 8,
+	clust_size     = 3,
+	height_min     = -31000,
+	height_max     = 64,
+})
+minetest.register_craftitem("xytest:xyxhorium", {
+	description = "Xychorium",
+	inventory_image = "xytest_xychorium.png",
+})
+minetest.register_node("xytest:xychorium_brick_basic", {
+	description = "Basic Xychorium Brick",
+	tiles = {"xytest_brick_basic.png"},
+	is_ground_content = true,
+	groups = {cracky=3},
 	sounds = default.node_sound_stone_defaults(),
 })
